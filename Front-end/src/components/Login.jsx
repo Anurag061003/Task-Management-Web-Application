242ba87e347726805d44aa2b310e32fef92dc316
Anurag061003 <anuragchauhan66317@gmail.com>
2026-07-17 12:24:34 +0530

    Update API URL in Login component

--- Front-end/src/components/Login.jsx
+++ Front-end/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Spinner, Alert, Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl ="https://task-management-backend-5wdj.onrender.com";
 
 const Login = () => {
   const navigate = useNavigate();
