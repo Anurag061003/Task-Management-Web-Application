88ea05aaaa2616a2f17be2718eca063609ea615b
Anurag061003 <anuragchauhan66317@gmail.com>
2026-07-17 13:41:33 +0530

    Update API URL for task management backend

--- Front-end/src/components/TaskList.jsx
+++ Front-end/src/components/TaskList.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Spinner, Button, Modal } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../Navbar.jsx";
-const apiUrl = "https://task-management-backend-5wdj.onrender.com";
+const apiUrl = "https://task-management-backend-pu7x.onrender.com";
 
 const TaskList = () => {
   const navigate = useNavigate();
