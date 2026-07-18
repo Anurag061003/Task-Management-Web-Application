dba506fcc3c29c95187a66cabbadc64c80b6ecef
Anurag061003 <anuragchauhan66317@gmail.com>
2026-07-18 13:59:02 +0530

    Update API URL in Login component

--- Front-end/src/components/Login.jsx
+++ Front-end/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Spinner, Alert, Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-const apiUrl ="https://task-management-backend-pu7x.onrender.com";
+const apiUrl ="https://task-management-backend-u92m.onrender.com";
 
 const Login = () => {
   const navigate = useNavigate();
